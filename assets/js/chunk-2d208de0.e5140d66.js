a8bacb71da7c2fd11a89851441be7ea527247aa4
claudiabdm <claudiabdm@hotmail.es>
2021-09-13 21:00:01 +0200

    deploy

---
+++ assets/js/chunk-2d208de0.e5140d66.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d208de0"],{a72f:function(n,e,t){"use strict";t.r(e),t.d(e,"amplify_container",(function(){return r}));var c=t("b70c"),i=":host{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;-ms-flex-pack:center;justify-content:center;height:100vh}",r=function(){function n(n){Object(c["k"])(this,n)}return n.prototype.render=function(){return Object(c["i"])(c["b"],null,Object(c["i"])("slot",null))},n}();r.style=i}}]);
+//# sourceMappingURL=chunk-2d208de0.e5140d66.js.map
---
+++ precache-manifest.7d6f449e8b449ec73e9e720603255847.js
@@ -0,0 +1,194 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "789a30f9ee02898dc2a11b06703b319d",
+    "url": "/acnh-search/acnh-search-logo.png"
+  },
+  {
+    "revision": "cb4235cb9901ff85a083",
+    "url": "/acnh-search/assets/css/app.eb97f17b.css"
+  },
+  {
+    "revision": "a980dbd0279b01574ee6",
+    "url": "/acnh-search/assets/css/chunk-vendors.d399b23d.css"
+  },
+  {
+    "revision": "8aa0ab4926aab1d490776698703ad0f5",
+    "url": "/acnh-search/assets/fonts/ArialRoundedBold.8aa0ab49.ttf"
+  },
+  {
+    "revision": "62e763eac335f96dfa419619da38aead",
+    "url": "/acnh-search/assets/fonts/FinkHeavy.62e763ea.woff"
+  },
+  {
+    "revision": "796b643a146db4b34af5f263f2b19e4a",
+    "url": "/acnh-search/assets/fonts/FinkHeavy.796b643a.ttf"
+  },
+  {
+    "revision": "864f6bbbe6835947023203737e859028",
+    "url": "/acnh-search/assets/fonts/FinkHeavy.864f6bbb.woff2"
+  },
+  {
+    "revision": "66dc854e8e6da281f8beee081d633d70",
+    "url": "/acnh-search/assets/fonts/materialdesignicons-webfont.66dc854e.eot"
+  },
+  {
+    "revision": "8ac434c848d34646f73544f35a903a67",
+    "url": "/acnh-search/assets/fonts/materialdesignicons-webfont.8ac434c8.woff"
+  },
+  {
+    "revision": "8ed8f0719def123d77bf6ea8919b8d8a",
+    "url": "/acnh-search/assets/fonts/materialdesignicons-webfont.8ed8f071.ttf"
+  },
+  {
+    "revision": "c0f7978a13704c522aaf355a80a5d0b0",
+    "url": "/acnh-search/assets/fonts/materialdesignicons-webfont.c0f7978a.woff2"
+  },
+  {
+    "revision": "cb4235cb9901ff85a083",
+    "url": "/acnh-search/assets/js/app.205da459.js"
+  },
+  {
+    "revision": "59f4b6781f3dba877ece",
+    "url": "/acnh-search/assets/js/chunk-04e1b7df.a6c43e16.js"
+  },
+  {
+    "revision": "a61d7d75df55c98cc372",
+    "url": "/acnh-search/assets/js/chunk-2d0abfcf.30943587.js"
+  },
+  {
+    "revision": "52c8eb2b682096e029b4",
+    "url": "/acnh-search/assets/js/chunk-2d0b26e6.4a8da242.js"
+  },
+  {
+    "revision": "2e4534df109b2b03be4e",
+    "url": "/acnh-search/assets/js/chunk-2d0b2d3b.59db2475.js"
+  },
+  {
+    "revision": "b901c2047a11b6aaa600",
+    "url": "/acnh-search/assets/js/chunk-2d0b722b.d73ac9e5.js"
+  },
+  {
+    "revision": "f1c0245222aac87fc91a",
+    "url": "/acnh-search/assets/js/chunk-2d0c9398.14242c3d.js"
+  },
+  {
+    "revision": "cfd650f74bc24857ec91",
+    "url": "/acnh-search/assets/js/chunk-2d0cc657.620ee2ce.js"
+  },
+  {
+    "revision": "2eb38bb2220a7e57c0d2",
+    "url": "/acnh-search/assets/js/chunk-2d0cf6e3.f6abceeb.js"
+  },
+  {
+    "revision": "ee78369031fa62a17f35",
+    "url": "/acnh-search/assets/js/chunk-2d0d314e.2e950d76.js"
+  },
+  {
+    "revision": "0030685b4313e92ce8c6",
+    "url": "/acnh-search/assets/js/chunk-2d0d7838.9a6a2462.js"
+  },
+  {
+    "revision": "bf7c778692ea9749b368",
+    "url": "/acnh-search/assets/js/chunk-2d0d7c42.94a3c664.js"
+  },
+  {
+    "revision": "0d22efc6e4e357346965",
+    "url": "/acnh-search/assets/js/chunk-2d0dd0bc.91e8cfc8.js"
+  },
+  {
+    "revision": "62eb29c2ecd0e88ae0e0",
+    "url": "/acnh-search/assets/js/chunk-2d208de0.e5140d66.js"
+  },
+  {
+    "revision": "3696a7a094cfd1689b89",
+    "url": "/acnh-search/assets/js/chunk-2d209182.4fdfa0fa.js"
+  },
+  {
+    "revision": "e38a459503b805bfdba8",
+    "url": "/acnh-search/assets/js/chunk-2d21097e.a5846c2b.js"
+  },
+  {
+    "revision": "f80ced8e3dc85bebc21a",
+    "url": "/acnh-search/assets/js/chunk-2d217172.bded37bf.js"
+  },
+  {
+    "revision": "f080f7e7523863761abe",
+    "url": "/acnh-search/assets/js/chunk-2d21d0be.b2683e4a.js"
+  },
+  {
+    "revision": "de3c5b4503833048bc60",
+    "url": "/acnh-search/assets/js/chunk-2d228cf2.d581ddc1.js"
+  },
+  {
+    "revision": "99c739f8811da02e28af",
+    "url": "/acnh-search/assets/js/chunk-2d229620.270feccb.js"
+  },
+  {
+    "revision": "076344de67b4353f7bbe",
+    "url": "/acnh-search/assets/js/chunk-2d22c8fe.18c9e89b.js"
+  },
+  {
+    "revision": "df4aad76905685202815",
+    "url": "/acnh-search/assets/js/chunk-2d22dbc9.c445184c.js"
+  },
+  {
+    "revision": "02f4cdc901296c487fd5",
+    "url": "/acnh-search/assets/js/chunk-2d23154f.7cb593c9.js"
+  },
+  {
+    "revision": "346d7c7d05c10a753b0a",
+    "url": "/acnh-search/assets/js/chunk-2d238683.53a5125a.js"
+  },
+  {
+    "revision": "633917fb1dbb70537b41",
+    "url": "/acnh-search/assets/js/chunk-414205a2.739d10e4.js"
+  },
+  {
+    "revision": "145f56bdaf513d64cb90",
+    "url": "/acnh-search/assets/js/chunk-49f714e4.e52f6d6d.js"
+  },
+  {
+    "revision": "5bf5ed61066a3c8619c9",
+    "url": "/acnh-search/assets/js/chunk-e5e8c26e.9c6c9c21.js"
+  },
+  {
+    "revision": "16225e61d9b78c59d2b4",
+    "url": "/acnh-search/assets/js/chunk-e707ffa4.2a3b3bcb.js"
+  },
+  {
+    "revision": "7cf73af5626ed2c71861",
+    "url": "/acnh-search/assets/js/chunk-f99768a2.ca7b8fb7.js"
+  },
+  {
+    "revision": "a980dbd0279b01574ee6",
+    "url": "/acnh-search/assets/js/chunk-vendors.d2f2fb52.js"
+  },
+  {
+    "revision": "864ef1ae85abe6f3d469",
+    "url": "/acnh-search/assets/js/polyfills-core-js.e53199db.js"
+  },
+  {
+    "revision": "76a11573888ef6f73e79",
+    "url": "/acnh-search/assets/js/polyfills-css-shim.2b8edf21.js"
+  },
+  {
+    "revision": "fdd23ce82c5f3606aa71",
+    "url": "/acnh-search/assets/js/polyfills-dom.4037579e.js"
+  },
+  {
+    "revision": "0ff77c2702c430a5fe8146caca945e06",
+    "url": "/acnh-search/assets/svgs/sprite.svg"
+  },
+  {
+    "revision": "77c35f96ec3add9ce622eefacad10e6d",
+    "url": "/acnh-search/index.html"
+  },
+  {
+    "revision": "15bc361e4eac000c4c2ca64a3e4ecf06",
+    "url": "/acnh-search/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/acnh-search/robots.txt"
+  }
+]);
